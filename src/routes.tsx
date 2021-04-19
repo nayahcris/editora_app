051ffc7b64cc440c568e7ae9fbf12a7b84ebcf3a
NAYARA CRISTINE BRAGA PEREIRA <57842812+nayahcris@users.noreply.github.com>
2021-04-19 19:28:27 -0300

    #5

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom'
 import Home from '../src/pages/Home'
 import Tasks from '../src/pages/Tasks'
-import Conto from "./pages/Conto";
-import Contos from "./pages/Contos";
+import Conto from "./pages/Conto"
+import Contos from "./pages/Contos"
+import Conectese from './pages/Conectese/index'
+
 
 const Routes: React.FC = () => {
-    return (
+   return (
         <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/tarefas" component={Tasks} />
             <Route path="/contos" component={Contos} />
             <Route path="/conto" component={Conto} />
+            <Route path="/conectese" component={Conectese} />   
         </Switch>
     )
 }
