b2142eff39d53017722cc50bb8e1ec179c9fc633
NAYARA CRISTINE BRAGA PEREIRA <57842812+nayahcris@users.noreply.github.com>
2021-04-25 17:43:36 -0300

    #Modal adiocinado, necessita de mudanças

--- src/pages/WebAr/index.jsx
+++ src/pages/WebAr/index.jsx
@@ -1,21 +1,45 @@
 /* eslint-disable */
 import { AFrameRenderer, Marker } from '../../components'
+import { Modal, Button } from 'react-bootstrap'
+import { useHistory } from 'react-router-dom'
 import React from 'react'
 
+
+
 export default function ReactArApp (){
+    const history = useHistory()
+    function goBack(){
+      history.push("/conto")
+    }
     return (
-        <AFrameRenderer inherent={true}>
-        <Marker parameters={{ preset: "hiro" }}>
-          <a-box color="blue" position="0 0.09 0" scale="0.4 0.8 0.8">
-            <a-animation
-              attribute="rotation"
-              to="360 0 0"
-              dur="2000"
-              easing="linear"
-              repeat="indefinite"
-            />
-          </a-box>
-        </Marker>
-      </AFrameRenderer>
+        <div className="container">
+          <Modal.Dialog>
+            <Modal.Header closeButton>
+              <Modal.Title>REALIDADE AUMENTADA</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+                <AFrameRenderer inherent={true}>
+                  <Marker parameters={{ preset: "hiro" }}>
+                    <a-box color="red" position="0 0.09 0" scale="0.4 0.8 0.8">
+                      <a-animation
+                        attribute="rotation"
+                        to="360 0 0"
+                        dur="2000"
+                        easing="linear"
+                        repeat="indefinite"
+                     />
+                   </a-box>
+                  </Marker>
+                </AFrameRenderer>
+            </Modal.Body>
+
+            <Modal.Footer>
+              <Button variant="secondary" onClick={goBack}>Fechar</Button>
+            </Modal.Footer>
+          </Modal.Dialog>
+           
+        </div>
+       
     )
 }
