c6f4fb9c3e44260d81b722b3de51754652f1f0a7
NAYARA CRISTINE BRAGA PEREIRA <57842812+nayahcris@users.noreply.github.com>
2021-05-27 14:29:25 -0300

    #Melhorias com QRCODE

---
+++ src/pages/Qrcode/index.jsx
@@ -0,0 +1,48 @@
+import {Grid, Button, Typography} from '@material-ui/core';
+import Icon from '@mdi/react'
+import { mdiQrcode, mdiQrcodeScan } from '@mdi/js';
+import { Link } from "react-router-dom";
+
+function HomeQR() {
+
+    return (
+        <div>
+
+            <Typography style={{margin:30}} variant="h2">
+            QRCODE
+            </Typography>
+
+            <Grid container spacing={6}>
+                <Grid item xs={6}>
+                    <Link to="/qr_generator">
+                    <Button variant="contained" size="large" color="primary">
+                        <Icon 
+                        style={{padding:10}}
+                        path={mdiQrcode}
+                        title="QR Generator"
+                        size={10}
+                        color="white"
+                        />
+                    </Button>
+                    </Link>
+                </Grid>
+                <Grid item xs={6}>
+                    <Link to="/qr_scanner">
+                    <Button variant="contained" size="large" color="primary">
+                        <Icon 
+                        style={{padding:10}}
+                        path={mdiQrcodeScan}
+                        title="QR Scanner"
+                        size={10}
+                        color="white"
+                        />
+                    </Button>
+                    </Link>
+                </Grid>
+            </Grid>
+        
+        </div>
+    );
+  }
+  
+  export default HomeQR
